c3afae4a39e0a9e1d4756e99e2681f375d073d35
sandokan <sandokandias@gmail.com>
2021-12-02 15:31:06 -0300

    fix append strategy

--- .github/scripts/append_plugin.py
+++ .github/scripts/append_plugin.py
@@ -1,11 +1,22 @@
 import json
 import os
 
-payload = os.getenv('EVENT_PAYLOAD')
-plugin = json.loads(payload)
+payload = json.loads(os.getenv('EVENT_PAYLOAD'))
+tag = payload["tag"]
+plugin = payload["plugin"]
+plugin["tags"] = [ tag ]
+
 with open('./plugins.json', 'rt') as prf:
         plugins = json.load(prf)
-        plugins.extend(plugin)
+        if not plugins:
+                plugins.append(plugin)
+        else:
+                for p in plugins:
+                        if p["name"] == plugin["name"]:
+                                if tag not in p["tags"]:
+                                        p["tags"].append(tag)
+                                else: raise Exception(f'plugin with tag {tag} already indexed')
+                        else: plugins.append(plugin)
 
 with open('./plugins.json', 'wt', encoding='utf-8') as pwf:
         json.dump(plugins, pwf, ensure_ascii=False, indent=4)
